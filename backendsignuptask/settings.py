dded7fbf69805f842dbb86e8b86524f8f12aa80e
Muhammad Murtaza <48488384+murtaza7799@users.noreply.github.com>
2025-04-07 14:00:27 +0500

    Add static file settings and Whitenoise middleware for production

--- backendsignuptask/settings.py
+++ backendsignuptask/settings.py
@@ -35,6 +35,14 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+# This setting informs Django of the URI path from which your static files will be served to users
+# Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
+STATIC_URL = '/static/'
+# This production code might break development mode, so we check whether we're in DEBUG mode
+if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -51,6 +59,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
